43bcad0fdb54596d7eb12ffdfe07e2ab0f60eea7
Chris Snyder <csnyder332@gmail.com>
2022-05-15 13:53:30 -0500

    more updates

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Card, Button, CardGroup, Container } from "react-bootstrap";
-import Image from "../../components/Projects/bootlegss.png";
-import Image1 from "../../components/Projects/calorie-counter.png";
-import Image2 from "../../components/Projects/ConstServices.png";
-import Image3 from "../../components/Projects/Comingsoon2.png";
+import Image from "../Projects/bootlegss.png";
+import Image1 from "../Projects/calorie-counter.png";
+import Image2 from "../Projects/ConstServices.png";
+import Image3 from "../Projects/Comingsoon2.png";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Projects() {
@@ -16,7 +16,7 @@ function Projects() {
           <Card.Body>
             <Card.Title>bootleg</Card.Title>
             <Card.Text>A photo sharing app.</Card.Text>
-            <Button>Close</Button>
+            <Button variant="primary p-1 p-sm-2" href="https://group-project-bootleg.herokuapp.com/" title="Bootleg">Open page</Button>
           </Card.Body>
         </Card>
         <Card>
@@ -24,7 +24,7 @@ function Projects() {
           <Card.Body>
             <Card.Title>Calorie Counter</Card.Title>
             <Card.Text>Calorie Counter</Card.Text>
-            <Button>Close</Button>
+            <Button variant="primary p-1 p-sm-2" href="https://csnyder332.github.io/Calorie-counter/" title="Bootleg">Open page</Button>
           </Card.Body>
         </Card>
         <Card>
@@ -32,7 +32,7 @@ function Projects() {
           <Card.Body>
             <Card.Title>Construction Services</Card.Title>
             <Card.Text>A full service painting/remodeling company.</Card.Text>
-            <Button>Close</Button>
+            <Button variant="primary p-1 p-sm-2" href="https://csnyder332.github.io/Jaqueline-site/" title="Bootleg">Open page</Button>
           </Card.Body>
         </Card>
         <Card>
@@ -40,7 +40,7 @@ function Projects() {
           <Card.Body>
             <Card.Title>Coming Soon</Card.Title>
             <Card.Text>Coming Soon.</Card.Text>
-            <Button>Close</Button>
+            <Button>Coming Soon</Button>
           </Card.Body>
         </Card>
       </CardGroup>
---
+++ src/components/Resume/resume.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+
+
+function Resume() {
+  return (
+      <div id="res"> 
+      <a href="../Resume/CSnyder0422.pdf">   
+    <FontAwesomeIcon icon="fa-solid fa-arrow-up-right-from-square" />
+    </a>
+    </div>
+  )
+};
+
+export default Resume;
